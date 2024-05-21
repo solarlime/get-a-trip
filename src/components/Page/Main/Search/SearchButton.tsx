5257e572f7a954ff8852dfaf3c5fec571522ccc1
solarlime <solarlime@outlook.com>
2024-05-21 21:13:15 +0300

    Now the search button is adaptive and always square

--- src/components/Page/Main/Search/SearchButton.tsx
+++ src/components/Page/Main/Search/SearchButton.tsx
@@ -1,13 +1,16 @@
-import { memo } from 'react';
+import { memo, MutableRefObject, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 import styles from '../../Page.module.scss';
 import useStore from '../../../../store/store';
+import useResizeObserver from './useResizeObserver';
 
 const SearchButton = memo((props: { disabled: boolean }) => {
+  const ref: MutableRefObject<HTMLAnchorElement | null> = useRef(null);
   const getFilteredTours = useStore((state) => state.getFilteredTours);
+  useResizeObserver(ref);
   const { disabled } = props;
 
   return (
@@ -15,6 +18,7 @@ const SearchButton = memo((props: { disabled: boolean }) => {
       className={`button is-primary is-flex-shrink-0 has-text-white ${disabled ? styles.disabled : ''}`}
       to="/directions#results"
       onClick={getFilteredTours}
+      ref={ref}
     >
       <FontAwesomeIcon className="is-size-6-mobile is-size-4" icon={faMagnifyingGlass} />
     </Link>
